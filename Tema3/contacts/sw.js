4ae82d6925430c04db6604352d0680f3718dd2ea
razvanpaduraru <razvan.paduraru@stud.acs.upb.ro>
2020-04-23 18:09:31 +0300

    Finished PWA

--- Tema3/contacts/sw.js
+++ Tema3/contacts/sw.js
@@ -1,1 +1,7 @@
-// ...
+self.addEventListener('install', event => {
+  console.log('Service worker has been installed.');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Service has fetched a resource.');
+});
