066ea136e8b7bf5feac086d16fe8714b186d06a7
razvanpaduraru <razvan.paduraru@stud.acs.upb.ro>
2020-04-30 20:03:52 +0300

    Todo Homework

---
+++ todo/src/index.js
@@ -0,0 +1,9 @@
+/*
+    c1: buton adaugare todo
+        input text si un buton langa
+    c2: un rand din lista de todo-uri
+        sa aiba un buton de remove alaturat
+        un todo sa fie stocat in localstorage
+    c3: lista de todo-uri
+    c4: componenta principala
+*/
